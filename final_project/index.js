087f8a1839efa27028dc00ee1ea7da548cc35f45
OlivierR63 <olivier.rochat@gmail.com>
2025-02-27 09:45:42 -0500

    Authentication code updated in index.js

--- final_project/index.js
+++ final_project/index.js
@@ -8,11 +8,41 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer",
+            session({
+                        secret:"fingerprint_customer",
+                        resave: true, 
+                        saveUninitialized: true
+                    }));
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
-});
+app.use("/customer/auth/*", 
+            function auth(req,res,next)
+            {
+                // Check if the user has a session with an authorization token
+                if (req.session.authorization && req.session.authorization.accessToken)
+                {
+                    const token = req.session.authorization.accessToken;
+
+                    // Verify the validity of the JWT token
+                    jwt.verify(token, "access_secret_key", (err, user) => {
+                        if (!err) {
+                            // If the token is valid, attach user information to the request object
+                            req.user = user;
+                            // Proceed to the next route handler
+                            next();
+                        } else {
+                            // If the token is invalid, send an error response
+                            return res.status(403).json({ message: "Invalid or expired token" });
+                        }
+                    });
+                } 
+                else
+                {
+                    // If no token is present, send an error response
+                    return res.status(403).json({ message: "User not authenticated" });
+                }
+            }
+        );
  
 const PORT =5000;
 
