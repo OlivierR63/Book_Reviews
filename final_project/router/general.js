0a2d0d986d5e258bd2826b0b4bfc221c849824b9
OlivierR63 <olivier.rochat@gmail.com>
2025-03-04 07:39:02 -0500

    implemented get_book_by_isbn and get_book_by_author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -57,47 +57,72 @@ public_users.get('/',(req, res)=> {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-    const isbn = req.params.isbn;
-    const book = books[isbn];
-    if (book) 
-    {
-        // If the book is found, return the book details.
-        return res.status(200).json({
-            isbn: isbn,
-            author: book.author,
-            title: book.title,
-            reviews: book.reviews,
-            reviewsCount: Object.keys(book.reviews).length // Number of reviews.
-        });
-    }
-    else
-    {
-        // If the book is not found, return a 404 status.
-        return res.status(404).json({ message: "Book not found" });
-    }
+public_users.get('/isbn/:isbn', (req, res)=>{
+    let get_book_based_on_isbn = new Promise((resolve, reject)=>{
+        try
+        {
+            let isbn = req.params.isbn;
+            let book = books[isbn];
+
+            // If the book is found, return the book details.
+            resolve( res.status(200).json({
+                isbn: isbn,
+                author: book.author,
+                title: book.title,
+                reviews: book.reviews,
+                reviewsCount: Object.keys(book.reviews).length // Number of reviews.
+            }));
+        }
+        catch(error)
+        {
+            reject(error);
+        }
     });
+
+    get_book_based_on_isbn.then(() =>{
+        console.log(`get_book_based_on_its_isbn: Promise resolved`);
+    })
+    .catch((error)=>{
+        // Return response with a status of 500 (server side error):
+        console.error('Error getting books:', error);
+        res.status(500).send("Unable to get book based on its ISBN");
+    });
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-    const authorName = req.params.author;
-    const booksByAuthorArray = Object.values(books).filter((book) => book.author.toLowerCase() === authorName.toLowerCase());
+    let get_book_by_author = new Promise((resolve, reject)=>{
+        try
+        {
+            let authorName = req.params.author;
+            let booksByAuthorArray = Object.values(books).filter((book) => book.author.toLowerCase() === authorName.toLowerCase());
 
-    if (booksByAuthorArray.length > 0) {
-        // If books by the author are found, return the book details.
-        const response = booksByAuthorArray.map((book, index) => ({
-            id: index,
-            author: book.author,
-            title: book.title,
-            reviews: booksByAuthorArray.reviews,
-            reviewsCount: Object.keys(book.reviews).length // Number of reviews.
-        }));
+            // If books by the author are found, return the book details.
+            let response = booksByAuthorArray.map((book, index) => ({
+                id: book.index,
+                author: book.author,
+                title: book.title,
+                reviews: booksByAuthorArray.reviews,
+                reviewsCount: Object.keys(book.reviews).length // Number of reviews.
+            }));
 
-        return res.status(200).json(response);
-    } else {
-        // If no books by the author are found, return a 404 status.
-        return res.status(404).json({ message: "No books found by this author" });
-    }
+            resolve(res.status(200).json(response));
+        }
+        catch(error)
+        {
+            // If no books by the author are found, return a 404 status.
+            reject(error);
+            res.status(404).send({ message: "No books found by this author" });
+        }
+    });
+
+    get_book_by_author.then(()=>{
+        console.log(`get_book_based_on_its_author: Promise resolved`);
+    })
+    .catch((error)=>{
+        console.error(`Error getting books:  ${error}`);
+        res.status(500).send("Unable to get book based on its author");
+    });
 });
 
 // Get all books based on title
